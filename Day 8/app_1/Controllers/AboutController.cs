9c654d53e645c8171987b17c923fa30443628e94
shaggy <someaim1@gmail.com>
2024-02-27 21:42:41 +0545

    Added New Files of MVC

---
+++ Day 8/app_1/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace app_1.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Day 8/app_1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using app_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace app_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUS() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
