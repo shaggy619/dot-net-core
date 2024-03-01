da1ddf1c313ab285050c1a43eae69a8a7d842b85
puja lakhe <lakhepuza924@gmail.com>
2024-03-01 10:34:38 +0545

    add linq and lambda

--- Day 7/linqvr1/ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
+++ Day 7/linqvr1/ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("linq")]
+[assembly: AssemblyTitle("ConsoleApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("linq")]
+[assembly: AssemblyProduct("ConsoleApp1")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0b6c891d-ee77-470c-a7f9-747520e87373")]
+[assembly: Guid("c2860a59-34c9-46f3-a756-18813637d591")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Day 7/lyamdaExpn/Program.cs
+++ Day 7/lyamdaExpn/Program.cs
@@ -11,40 +11,71 @@ namespace lyamdaExpn
     {
         static void Main(string[] args)
         {
-                List<string> list = new List<string>();
-           list = GetAppropriateList("pu");   //items Containing string 'pu'
-              //list = GetAppropriateList("n");//items that Starts with 'n'
-                 //list = GetAppropriateList("centric"); //items that are Equal to 'centric'
-                foreach (string subname in list)
-                {
-                     Console.WriteLine($"{subname}");
-                 
-                }
-                Console.ReadKey();
-        }
-           static List<string> GetAppropriateList(string subname)
+            List<string> list = new List<string>();
+            list = GetList();
+            List<string> list1 = new List<string>();
+            list1 = Contain("e", list);
+            List<string> list2 = new List<string>();
+            list2 = Equals("centric", list);
+            List<string> list3 = new List<string>();
+            list3 = StartWith("com", list);
+            Console.WriteLine("Following are the available lists:");
+            foreach (string subname in list)
             {
-                List<string> list = new List<string>();
-                list.Add("Net");
-                list.Add("Centric");
-                list.Add("Computing");
-                list.Add("NET");
-                list.Add("CENTRIC");
-                list.Add("COMPUTING");
-                list.Add("net");
-                list.Add("centric");
-                list.Add("computing");
-                list.Add("nET");
-                list.Add("cENtric");
-                list.Add("cOMPUTING");
-            return
-            //Equal to 
-            //list.Where(sub => sub.ToUpper() == subname.ToUpper()).ToList();
-            //starts with
-            //list.Where(sub => sub.ToUpper().StartsWith(subname.ToUpper())).ToList();
-            //Contains
-            list.Where(sub => sub.ToUpper().Contains(subname.ToUpper())).ToList();
+                Console.WriteLine($"{subname}");
+                 
+            }
+            Console.WriteLine("String Containing 'e' are:");
+            foreach (string subname in list1)
+            {
+                Console.WriteLine($"{subname}");
 
+            }
+            Console.WriteLine("String equals to 'centric' are:");
+            foreach (string subname in list2)
+            {
+                Console.WriteLine($"{subname}");
+
+            }
+            Console.WriteLine("String that starts with 'com' are:");
+            foreach (string subname in list3)
+            {
+                Console.WriteLine($"{subname}");
+
+            }
+            Console.ReadKey();
+        }
+        static List<string> GetList()
+        {
+            List<string> list = new List<string>();
+            list.Add("Net");
+            list.Add("Centric");
+            list.Add("Computing");
+            list.Add("NET");
+            list.Add("CENTRIC");
+            list.Add("COMPUTING");
+            list.Add("net");
+            list.Add("centric");
+            list.Add("computing");
+            list.Add("nET");
+            list.Add("cENtric");
+            list.Add("cOMPUTING");
+            return list;
+        }
+        //Contains
+        static List<string> Contain(string subname,List<string> list)
+        {
+            return list.Where(sub => sub.ToUpper().Contains(subname.ToUpper())).ToList();
+        }
+        //Equal to
+        static List<string> Equals(string subname, List<string> list)
+        {
+            return list.Where(sub => sub.ToUpper() == subname.ToUpper()).ToList();
+        }
+        //starts with
+        static List<string>StartWith(string subname,List<string> list)
+        {
+            return list.Where(sub => sub.ToUpper().StartsWith(subname.ToUpper())).ToList();
         }
     }
 }
